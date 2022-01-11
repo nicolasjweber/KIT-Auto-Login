c44b76bb2aaae8699ade5edde575b90e52747c84
BenedictLoe <97554733+BenedictLoe@users.noreply.github.com>
2022-01-11 20:47:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Ilias Login
+
+This is a repository for a **firefox** addon which clicks you through the ilias and campus pages automatically. It is meant to be used by people studying at KIT University in Karlsruhe Germany. You need to have your password saved by the firefox password manager.
+
+## How to use:
+
+Firefox addon Link: https://addons.mozilla.org/de/firefox/addon/iliaslogin/
+
+## Wanna help?
+
+You are welcome to make a push request. But if you prefer you can also just fork it, I do not mind at all.
+
+Original by phillip > https://addons.mozilla.org/de/firefox/addon/iliaslogin/
+Just a quick fix for Ilias 7, very dirty, don't know anything about javascript :)
---
+++ iliasLogin.js
@@ -0,0 +1,82 @@
+/// script to go through the ilias and campus pages
+
+//Original by phillip > https://addons.mozilla.org/de/firefox/addon/iliaslogin/
+//Just a quick fix for Ilias 7, very dirty, don't know anything about javascript :)
+
+if (window.location.href.indexOf("ilias.studium.kit.edu/login") > -1) {
+  console.log("TestSimple URL detected");
+  //clickButtonById('f807');
+  clickButtonById('button_shib_login');
+} else if (window.location.href.indexOf("campus.studium.kit.edu") > -1) {
+  console.log("Campus URL detected");
+  setTimeout(function () { clickButtonByClass('shib-login shib-button'); }, 100); //
+} else if (window.location.href.indexOf("idp.scc.kit.edu") > -1) {
+  // Careful, here chrome doesn't load the passwords in properly (Not exactly clear why)
+  // Before clicking on submit, we will use a trick and click on some text beforehand.
+
+  // only text, not an actual button. Index = 1 because with Index = 0 
+  // is a class with a button in it which we do not want to click on
+  clickButtonByClass('text full', 1);
+  
+  clickButtonById('sbmt'); // the actual button
+} else if (detectMoreComplexUrl()) {
+  console.log("Complex URL detected");
+ /// clickButtonByClass('btn btn-bulky',2);
+  clickListElement("il-maincontrols-metabar", 2)
+}
+
+else if (window.location.href.toLowerCase().indexOf("https://ilias.studium.kit.edu/login.php?client_id=produktiv&cmd=force_login&lang=") > -1) {
+    console.log("New login page")
+	clickButtonById('button_shib_login');
+  }
+
+
+else if (window.location.href.indexOf("https://ilias.studium.kit.edu/login.php?target=root_1&client_id=produktiv&cmd=force_login&lang=de") > -1) {
+  console.log("Button Page URL detected");
+  clickButtonById("button_shib_login");
+  console.log("Clicked Button");
+ // clickButtonByClass('btn btn-bulky',2);
+  //clickListElement("il-maincontrols-metabar", 2)
+}
+
+function detectMoreComplexUrl() {
+  // return false for an URL to a forum Link
+  if (window.location.href.toLowerCase().indexOf("https://ilias.studium.kit.edu/ilias.php?baseclass=ilrepositorygui&cmd=viewthread") > -1) {
+    console.log("Forum Link detected, IliasLogin not executing because you should already be logged in.")
+    return false
+  }
+
+  // return true for any other URL which links to Magazin but is not a forum URL. If you are already logged in, the login Button is not there so it won't fire
+  if (window.location.href.toLowerCase().indexOf("https://ilias.studium.kit.edu/ilias.php?baseclass=ilrepositorygui") > -1) {
+    console.log("Magazin URL detected, Extensions firering")
+    return true
+  }
+}	
+
+function clickButtonById(id) {
+  document.getElementById(id).click();
+  console.log("button clicked");
+}
+
+function clickButtonByClass(className, index = 0) {
+  var shibButton = document.getElementsByClassName(className)[index];
+  console.log("button about to be clicked: " + shibButton);
+  shibButton.click();
+  console.log("button clicked");
+}
+
+function clickListElement(listClass, elementNumber) {
+  console.log("Replacing URL")
+  //setTimeout(function (){
+  
+  window.location.replace("https://ilias.studium.kit.edu/login.php?target=root_1&client_id=produktiv&cmd=force_login&lang=de");
+            
+//}, 100);
+  
+  //tagNameListElement = "li"
+  //urlElement = "a"
+  ///var listElement = document.getElementByClassName(listClass).getElementsByTagName(tagNameListElement)[elementNumber].getElementsByTagName(urlElement)[0]
+  //console.log("listElement URL: " + listElement + " about to be clicked")
+  //listElement.click()
+  console.log("listElement URL clicked on")
+}
